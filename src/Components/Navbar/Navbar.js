44807d60683f26ded258fa1ed8871870835ffc82
Kashish <kashishbhandula112@gmail.com>
2022-09-15 17:48:17 +0530

    Navbar

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+// rfce
+
+import React from "react";
+import './Navbar.css'
+
+function Navbar() {
+  return (
+    <div className="n-wrapper">
+      <div className="n-left">
+        <div className="n-name">Kashish</div>
+        <span>toggle</span>
+      </div>
+      <div className="n-right">
+        <div className="n-list">
+          <ul>
+            <li>Home</li>
+            <li>Service</li>
+            <li>Experience</li>
+            <li>Portfolio</li>
+            <li>Testimonial</li>
+          </ul>
+        </div>
+        <button className="button n-button">
+            Contact us
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
