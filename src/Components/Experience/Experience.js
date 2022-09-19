7373065fcca2a3620bd0ec0f6a28f156b116bcd3
Kashish <kashishbhandula112@gmail.com>
2022-09-19 19:39:43 +0530

    Achievement

---
+++ src/Components/Experience/Experience.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Experience.css'
+function Experience() {
+  return (
+    <div className='experience'>
+        <div className='achievement'>
+            <div className='circle'>0</div>
+            <span>Years</span>
+            <span>Experience</span>
+        </div>
+        <div className='achievement'>
+            <div className='circle'>10+</div>
+            <span>Completed</span>
+            <span>Projects</span>
+        </div>
+        <div className='achievement'>
+            <div className='circle'>1000+</div>
+            <span>Dsa</span>
+            <span>Questions</span>
+        </div>
+    </div>
+  )
+}
+
+export default Experience
--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,23 +1,56 @@
 import React from "react";
-import "./Services.css"
+import "./Services.css";
+import Card from "../Card/Card";
 import HeartEmoji from "../../img/heartemoji.png";
 import Glasses from "../../img/glasses.png";
 import Humble from "../../img/humble.png";
+import Resume from "./Resume.pdf";
 
 function Services() {
   return (
     <div className="services">
       {/* left side */}
-      
+
       <div className="awesome">
         <span>My Awesome</span>
         <span>services</span>
-        <button className="button s-button">Download CV</button>
-        <div className="blur s-blur" style={{background:"#ABF1FF94"}}></div>
+        <span style={{ marginTop: "1rem", color: "grey" }}>
+          bbbbbbbbbbbbbbbbbbbbbnnnnnnnnnnnbbbbbbbbbbbbbbbbbbbbbbbbb
+          <br />
+          bbbbbbbbbbbbbbbbbbbb
+        </span>
+        <a href={Resume} download>
+          <button className="button s-button">Download CV</button>
+        </a>
+        <div className="blur s-blur" style={{ background: "#ABF1FF94" }}></div>
       </div>
       {/* right side */}
       <div className="cards">
-        {/* I am the right side */}
+        <div style={{ left: "14rem" }}>
+          <Card
+            emoji={HeartEmoji}
+            heading={"Design"}
+            details={"bla blab ba; bandnlabsabbndnmasdmnnmdnmd"}
+          />
+        </div>
+        <div style={{ top: "12rem", left: "-4rem" }}>
+          <Card
+            emoji={Glasses}
+            heading={"Devloper"}
+            details={"bla blab ba; bandnla"}
+          />
+        </div>
+        <div style={{ top: "19rem", left: "12rem" }}>
+          <Card
+            emoji={Humble}
+            heading={"Competitive Programmer"}
+            details={"bla blab ba; bandnla"}
+          />
+        </div>
+        <div
+          className="blur s-blur2"
+          style={{ background: "var(--purple)" }}
+        ></div>
       </div>
     </div>
   );
