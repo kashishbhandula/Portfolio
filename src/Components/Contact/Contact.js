29cb6092a9ee6e9ec09ef777d3e9fa3df53ad57c
Kashish <kashishbhandula112@gmail.com>
2022-09-22 00:50:44 +0530

    Email js

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,51 +1,67 @@
-import React from 'react'
-import "./Contact.css"
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import "./Contact.css";
 function Contact() {
+  const form = useRef();
+  const [done, setDone] = useState(false);
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_wfc121w",
+        "template_fw6bqiu",
+        form.current,
+        "ZbNAuaJwcBpWyIjZo"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
   return (
-    <div className='contact-form'>
-        <div className='w-left'>
-            <div className='awesome'>
-                <span>Get in touch</span>
-                <span>Contact me</span>
-                <div 
-                    className='blur s-blur'
-                    style={{background:"#Abf1FF94"}}>
-
-
-                </div>
-            </div>
+    <div className="contact-form">
+      <div className="w-left">
+        <div className="awesome">
+          <span>Get in touch</span>
+          <span>Contact me</span>
+          <div
+            className="blur s-blur"
+            style={{ background: "#Abf1FF94" }}
+          ></div>
         </div>
-        <div className='c-right'>
-            <form>
-                <input 
-                    type="text"
-                    name='user_name'
-                    className='user'
-                    placeholder='Name'
-                />
-                <input 
-                    type="email"
-                    name='user_email'
-                    className='user'
-                    placeholder='Email'
-                />
-                <textarea
-                    
-                    name='message'
-                    className='user'
-                    placeholder='Message'
-                />
-                <input 
-                    type="submit"
-                    value='Send'
-                    className='button'
-                    
-                />
-            </form>
-
-        </div>
+      </div>
+      <div className="c-right">
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="user_name"
+            className="user"
+            placeholder="Name"
+          />
+          <input
+            type="email"
+            name="user_email"
+            className="user"
+            placeholder="Email"
+          />
+          <textarea name="message" className="user" placeholder="Message" />
+          <input type="submit" value="Send" className="button" />
+          <span>{done&&"Thanks for contacting me"}</span>
+          <div
+            className="blur c-blur"
+            style={{ background: "var(--purple)" }}
+          ></div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
